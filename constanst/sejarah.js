bc98fd62153f839436ad60c8b1e257f6a60d161c
Jefri Jaya <jefrijaya08@gmail.com>
2024-06-09 14:47:49 +0700

    Update sejarah.js

--- constanst/sejarah.js
+++ constanst/sejarah.js
@@ -4,16 +4,16 @@ export const sejarah = [
   {
     image: assets.sejarah1,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae reprehenderit exercitationem quod libero obcaecati atque illo quaerat explicabo nostrum maxime, pariatur architecto, nulla itaque veritatis possimus at assumenda eveniet delectus adipisci, autem necessitatibus vitae voluptatem! Ad facere accusamus et, amet facilis officia cum neque sequi odit ut maxime totam harum, minima at eligendi nihil fuga. Aperiam, numquam, exercitationem ducimus porro doloribus magni aliquid, tenetur pariatur praesentium libero dicta laudantium illo!",
+      "Indonesia Youth Economic Leaders yang biasa disebut IYEL, ialah organisasi kepemudaan nasional di bidang ekonomi dalam bentuk asosiasi. IYEL dibentuk pada tanggal 9 Juli 2023 di Jakarta oleh para alumni Indonesian Youth Economic Leadership  Program (YELP) 2023 yang terpanggil hati dan jiwanya untuk berbakti pada nusa dan bangsa melalui pembangunan ekonomi yang berkelanjutan yang diketuai oleh Kaisar Akhir.",
   },
   {
     image: assets.sejarah2,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae reprehenderit exercitationem quod libero obcaecati atque illo quaerat explicabo nostrum maxime, pariatur architecto, nulla itaque veritatis possimus at assumenda eveniet delectus adipisci, autem necessitatibus vitae voluptatem! Ad facere accusamus et, amet facilis officia cum neque sequi odit ut maxime totam harum, minima at eligendi nihil fuga. Aperiam, numquam, exercitationem ducimus porro doloribus magni aliquid, tenetur pariatur praesentium libero dicta laudantium illo!",
+      "Dengan ideologi dan gagasan untuk mendorong generasi muda menyadari perannya dalam membangun daya saing dan kemandirian ekonomi Indonesia sebagai Negara Nusantara, para pemuda ini bersatu mendirikan IYEL. Organisasi ini bergerak atas latar belakang akademis, keilmuan bisnis, dan mengacu pada berbagai kebijakan serta kondisi kekinian dan kedisinian ekonomi berkelanjutan Indonesia.",
   },
   {
     image: assets.sejarah3,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae reprehenderit exercitationem quod libero obcaecati atque illo quaerat explicabo nostrum maxime, pariatur architecto, nulla itaque veritatis possimus at assumenda eveniet delectus adipisci, autem necessitatibus vitae voluptatem! Ad facere accusamus et, amet facilis officia cum neque sequi odit ut maxime totam harum, minima at eligendi nihil fuga. Aperiam, numquam, exercitationem ducimus porro doloribus magni aliquid, tenetur pariatur praesentium libero dicta laudantium illo!",
+      "IYEL memberanikan diri untuk dapat langsung berkomunikasi dan bekerja sama dengan berbagai elemen, baik pemerintah maupun swasta. Hasilnya, IYEL mendapatkan sambutan positif dalam pergerakannya di bidang bisnis dan ekonomi berkelanjutan. IYEL diterima sebagai organisasi kepemudaan ekonomi yang kaya akan intelektualitas kebumian dan cukup kritis dalam menanggapi isu-isu strategis pengelolaan dan pemanfaatan sumber daya alam dan jasa serta turut serta dalam bentuk aksi nyata kegiatan pendidikan, penelitian, dan pengabdian kepada masyarakat. Dari sinilah IYEL berangkat mengisi ruang kosong kepemudaan ekonomi berkelanjutan yang memang masih minim kontribusi dan gagasan.",
   },
 ];
