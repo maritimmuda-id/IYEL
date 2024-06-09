4958dd80fc58b021d2a39e7de9fd1a0c059eba9f
Jefri Jaya <jefrijaya08@gmail.com>
2024-06-09 18:30:08 +0700

    Update pengurus.js

--- constanst/pengurus.js
+++ constanst/pengurus.js
@@ -4,7 +4,43 @@ export const administrators = [
   {
     image: assets.kaisarImage,
     name: "Kaisar Akhir",
-    position: "Wakil Ketua Umum",
+    position: "Ketua Umum",
   },
+  {
+    image: assets.endangImage,
+    name: "Endang Dzunuraini",
+    position: "Direktur Ekonomi Kreatif",
+  },
+  {
+    image: assets.azizahImage,
+    name: "Azizah Fauziah Misbahuddin",
+    position: "Direktur ekononomi digital",
+  },
+  {
+    image: assets.nazriImage,
+    name: "Muhammad Nazri Syahputra ",
+    position: "Direktur Ekonomi Hijau",
+  },
+  {
+    image: assets.dwiImage,
+    name: "Dwi Grawana Chalista",
+    position: "Wasekjen Ekonomi Biru",
+  },
+  {
+    image: assets.julianImage,
+    name: "Julian Gumay",
+    position: "Bendahara Umum",
+  },
+  {
+    image: assets.hananImage,
+    name: "Hanan_Syauqy",
+    position: "Direktur Ekonomi Kesehatan",
+  },
+  {
+    image: assets.roikhanatunImage,
+    name: "Roikhanatun Nafi'ah",
+    position: "Direktur Ekonomi Biru",
+  },
+  
 
 ];
