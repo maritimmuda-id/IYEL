1884d014b212ae407270cff2a9e4fa4ebc717606
Jefri Jaya <jefrijaya08@gmail.com>
2024-06-09 14:47:44 +0700

    Update lambang.js

--- constanst/lambang.js
+++ constanst/lambang.js
@@ -1,18 +1,31 @@
 export const lambang = [
   {
-    title: "Warna Biru, Hijau, Orange",
+    title: "Bentuk, Warna, Font",
     description: [
       {
-        list: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem facere mollitia aperiam quaerat accusamus! Sunt reiciendis labore iure qui molestias rerum, delectus illo esse iusto nam, quos nobis. Nobis, repudiandae!",
+        list: "Bentuk Lingkaran: Lingkaran dalam logo ini sering kali melambangkan kesatuan, kontinuitas, dan globalisasi. Ini bisa menggambarkan persatuan pemuda Indonesia dalam mencapai tujuan ekonomi yang berkelanjutan."
       },
-    ],
+      {
+        list: "Warna Hijau dan Biru: Warna hijau sering dikaitkan dengan pertumbuhan, alam, dan keberlanjutan, sementara warna biru melambangkan kepercayaan, stabilitas, dan tanggung jawab. Kombinasi ini menunjukkan fokus IYEL pada pertumbuhan ekonomi yang berkelanjutan dan bertanggung jawab."
+      },
+      {
+        list: "Font: Font yang bersih dan modern menunjukkan profesionalisme dan keterbukaan terhadap inovasi."
+      }
+
+    ]
   },
   {
-    title: "Tulisan IYEL",
+    title: "Elemen Desain",
     description: [
       {
-        list: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem facere mollitia aperiam quaerat accusamus! Sunt reiciendis labore iure qui molestias rerum, delectus illo esse iusto nam, quos nobis. Nobis, repudiandae!",
+        list: "Simbol dalam Lingkaran: Simbol berbentuk abstrak di dalam lingkaran tampak seperti dua elemen yang saling berhubungan. Ini bisa melambangkan kolaborasi dan sinergi antara berbagai elemen atau entitas dalam mencapai tujuan ekonomi."
       },
-    ],
-  },
+      {
+        list: "Garis Melengkung: Garis melengkung yang mengelilingi simbol dalam lingkaran bisa melambangkan dinamisme dan gerakan maju, mencerminkan semangat inovasi dan perkembangan."
+      },
+      {
+        list: "IYEL: Singkatan dari Indonesian Youth Economic Leaders yang menjelaskan bahwa organisasi ini berfokus pada kepemimpinan ekonomi pemuda Indonesia."
+      }
+    ]
+  }
 ];
